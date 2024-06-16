9472fb1a5392c32d3813bd5be2ebe8b2adf4b4e8
Zeeshan Mustafa <zeeshan.mustafa91@gmail.com>
2024-06-16 11:21:02 +0500

    Prompt templating with personas and message roles

--- PromptTemplatingWithPersonas.cs
+++ PromptTemplatingWithPersonas.cs
@@ -29,7 +29,46 @@ internal class PromptTemplatingWithPersonas
             new KernelArguments() { { "history", history }, { "language", language } });
 
 
-        Console.WriteLine(result);
+        Console.WriteLine(result + "\n\n");
 
+        //test personas with roles
+        string prompt2 = @"
+         The following is a conversation with an AI travel assistant. 
+         The assistant is helpful, creative, and very friendly. Always responds in respectful and professional tone.
+
+        <message role=""user"">Can you give me some travel destination suggestions?</message>
+        <message role=""assistant"">Of course! Do you have a budget or any specific activites in mind?</message>
+        <message role=""user"">{{$input}}</message>
+        ";
+
+        string input = "I'm planning an anniversary trip with my spouse. We like hiking, mountains," +
+                        "and beaches. Our travel budget is $15000";
+
+
+        result = await kernel.InvokePromptAsync(prompt2, 
+            new KernelArguments() { { "input", input } });
+
+        Console.WriteLine(result + "\n\n");
+
+
+        //format response
+        string prompt3 = @"
+        <message role=""system"">Instructions: Identify the from and to destinations 
+        and dates from the user's request</message>
+        
+        <message role=""user"">Can you give me a list of flights from Seattle to Tokyo? 
+        I want to travel from March 11 to March 18.</message>
+        
+        <message role=""assistant"">Seattle|Tokyo|03/11/2024|03/18/2024|Turkish Airlines</message>
+        
+        <message role=""user"">{{$input}}</message>";
+
+        input = @"I have a vacation from June 1 to July 22. I want to go to Greece. 
+        I live in Chicago.";
+
+        result = await kernel.InvokePromptAsync(prompt3,
+            new KernelArguments() { { "input", input } });
+
+        Console.WriteLine(result + "\n\n");
     }
 }
