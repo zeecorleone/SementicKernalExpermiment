5d3c3ca92254fe28b74120761027f170c7aaeed4
Zeeshan Mustafa <zeeshan.mustafa91@gmail.com>
2024-08-17 09:20:11 +0500

    [Huggingface experiment] in progress

---
+++ SK-HuggingFace-Experiment/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel;
+
+namespace SK_HuggingFace_Experiment
+{
+    internal class Program
+    {
+        static async Task Main3(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+#pragma warning disable SKEXP0070 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+
+            var kernel = Kernel.CreateBuilder()
+                .AddHuggingFaceChatCompletion("gpt2",
+                    endpoint: new Uri("https://api-inference.huggingface.co/models/openai-community/gpt2"),
+                    apiKey: Environment.GetEnvironmentVariable("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"))
+                .Build();
+
+            // var result = await kernel.InvokePromptAsync("Hi, who are you?");
+
+            var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+            var result = await chatCompletionService.GetChatMessageContentsAsync("Hi, who are you?",
+                new PromptExecutionSettings() { ModelId = "gpt2" },
+                kernel);
+
+
+
+#pragma warning restore SKEXP0070 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+        }
+    }
+}
